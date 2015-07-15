cc0d36f372b8061f3d17632ae072dbbc4a549b6e
benjlevine <ben@point5.com>
2015-07-15 00:57:50 -0400

    addle cleanup id names

--- js/index.js
+++ js/index.js
@@ -9,20 +9,18 @@ $(function() {
 
 		// Save form submit to ENTRY
 		var inputValue = $('input#newEntry').val();
-		console.log('inputValue: ' + inputValue)
 		var entry = parseFloat(inputValue);
 		console.log('entry: ' + entry);
 
 		// Print ENTRY to the bottom of the ticker tape
-		$('tbody#entries').append('<tr><td colspan="2">$' + entry.toFixed(2) + '</td></tr>');
+		$('#entries').append('<tr><td colspan="2">$' + entry.toFixed(2) + '</td></tr>');
 
 		// Add ENTRY to TOTAL
-		console.log('total: ' + total);
     total = total + entry;
 		console.log('total: ' + total);
 
 		// Print TOTAL
-		$('th#total').html('$' + total.toFixed(2));
+		$('#total').html('$' + total.toFixed(2));
 
 		// Reset form
 		$('#entry').trigger('reset');
